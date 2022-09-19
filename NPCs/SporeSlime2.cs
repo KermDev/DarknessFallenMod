f66830240c388d980673f2b8b1ac5f8be85443eb
KermitDeveloper <ljpottspotter200@gmail.com>
2022-09-19 13:21:08 +0100

    spore slimes

---
+++ NPCs/SporeSlime2.cs
@@ -0,0 +1,67 @@
+using Terraria.ModLoader;
+using Terraria;
+using Terraria.ID;
+using Microsoft.Xna.Framework;
+using Terraria.ModLoader.Utilities;
+using DarknessFallenMod.Items.Materials;
+using Terraria.GameContent.ItemDropRules;
+using Terraria.GameContent.Bestiary;
+using System;
+using System.Collections.Generic;
+
+namespace DarknessFallenMod.NPCs
+{
+    public class SporeSlime2 : ModNPC
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("spore slime");
+            Main.npcFrameCount[NPC.type] = Main.npcFrameCount[2];
+        }
+
+        public override void SetDefaults()
+        {
+            NPC.width = 32;
+            NPC.height = 15;
+            NPC.damage = 11;
+            NPC.defense = 3;
+            NPC.lifeMax = 55;
+            NPC.value = 22f;
+            NPC.aiStyle = 1;
+            NPC.HitSound = SoundID.NPCHit1;
+            NPC.DeathSound = SoundID.NPCDeath1;
+            AIType = NPCID.GreenSlime;
+            AnimationType = NPCID.GreenSlime;
+            Banner = NPC.type;
+            BannerItem = ModContent.ItemType<Items.Placeable.Banners.SporeSlimeBanner2>();
+        }
+        public override float SpawnChance(NPCSpawnInfo spawnInfo)
+        {
+            return SpawnCondition.Cavern.Chance * 0.07f;
+        }
+
+        public override void FindFrame(int frameHeight)
+        {
+            NPC.frameCounter++;
+            if (NPC.frameCounter >= 20)
+            {
+                NPC.frameCounter = 0;
+            }
+            NPC.frame.Y = (int)NPC.frameCounter / 10 * frameHeight;
+        }
+
+        public override void ModifyNPCLoot(NPCLoot npcLoot)
+        {
+            npcLoot.Add(ItemDropRule.Common(ItemID.Mushroom, minimumDropped: 1, maximumDropped: 4));
+            npcLoot.Add(ItemDropRule.Common(ItemID.Gel, minimumDropped: 1, maximumDropped: 2));
+        }
+
+        public override void SetBestiary(BestiaryDatabase database, BestiaryEntry bestiaryEntry)
+        {
+            // Makes it so whenever you beat the boss associated with it, it will also get unlocked immediately
+            bestiaryEntry.Info.AddRange(new List<IBestiaryInfoElement> {
+                new FlavorTextBestiaryInfoElement("a slime that has traveled too far from the glowing mushrooms and has lost its glow")
+            });
+        }
+    }
+}
