69189a815dba2d2aa4f7769818a6dd6f7274b6cc
eatpotaro <eatpotaro@gmail.com>
2022-09-24 16:59:11 +0100

    added darknessfallenprojectile

---
+++ DarknessFallenProjectile.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace DarknessFallenMod
+{
+    public class DarknessFallenProjectile : GlobalProjectile
+    {
+        
+    }
+}
