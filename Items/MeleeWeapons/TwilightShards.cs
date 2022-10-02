4e6ee5b1c05d75eb653266594adc9530d2ad33b4
sucss <@sucss>
2022-10-02 20:04:10 +0200

    Flail finished (?)

--- Items/MeleeWeapons/TwilightShards.cs
+++ Items/MeleeWeapons/TwilightShards.cs
@@ -19,7 +19,7 @@ namespace DarknessFallenMod.Items.MeleeWeapons
     {
         public override void SetStaticDefaults()
         {
-            
+            Tooltip.SetDefault("");
         }
 
         public override void SetDefaults()
@@ -30,7 +30,7 @@ namespace DarknessFallenMod.Items.MeleeWeapons
             Item.knockBack = 6.75f;
             Item.width = 30;
             Item.height = 10;
-            Item.damage = 32;
+            Item.damage = 71;
             Item.crit = 7;
             Item.scale = 1.1f;
             Item.noUseGraphic = true;
@@ -38,11 +38,12 @@ namespace DarknessFallenMod.Items.MeleeWeapons
             Item.shootSpeed = 12f;
             Item.UseSound = SoundID.Item1;
             Item.rare = ItemRarityID.Orange;
-            Item.value = Item.sellPrice(gold: 2, silver: 50);
+            Item.value = Item.sellPrice(gold: 6);
             Item.DamageType = DamageClass.MeleeNoSpeed;
             Item.channel = true;
             Item.noMelee = true;
         }
+
     }
 
     public class TwilightShardsProjectile : Templates.FlailProjectile
@@ -57,24 +58,21 @@ namespace DarknessFallenMod.Items.MeleeWeapons
         {
             //Main.NewText($"AI0:{Projectile.ai[0]}, AI1:{Projectile.ai[1]}");
 
-            if (Main.netMode != NetmodeID.MultiplayerClient && !Owner.controlUseItem) 
+            if (Main.myPlayer == Owner.whoAmI && !Owner.controlUseItem && Projectile.ai[0] == 2) 
             {
-                var proj = Projectile.NewProjectileDirect(Projectile.GetSource_FromAI(), Projectile.Center, Projectile.velocity, ModContent.ProjectileType<TwilightShardsProjectileShoot>(), 20, 0, Projectile.owner);
-                proj.friendly = true;
-                proj.hostile = false;
-                proj.DamageType = DamageClass.MeleeNoSpeed;
+                Projectile.NewProjectile(Projectile.GetSource_FromAI(), Projectile.Center, Projectile.oldVelocity, ModContent.ProjectileType<TwilightShardsProjectileShoot>(), 71, 0.4f, Projectile.owner);
 
                 Projectile.Kill();
             }
         }
         
-
+        /*
         public override void OnHitNPC(NPC target, int damage, float knockback, bool crit)
         {
             SoundEngine.PlaySound(SoundID.Shatter with { Volume = 0.4f }, Projectile.Center);
 
             DarknessFallenUtils.NewDustCircular(Projectile.Center, DustID.Glass, 10, speedFromCenter: 6, amount: Main.rand.Next(10, 24)).ForEach(dust => dust.alpha = Main.rand.Next(0, 220));
-        }
+        }*/
     }
 
     public class TwilightShardsProjectileShoot : ModProjectile
@@ -83,20 +81,66 @@ namespace DarknessFallenMod.Items.MeleeWeapons
 
         public override void SetDefaults()
         {
-            Projectile.width = 15;
-            Projectile.height = 15;
-            Projectile.penetrate = 10;
-            Projectile.aiStyle = 1;
-            Projectile.DamageType = DamageClass.Melee;
+            Projectile.width = 30;
+            Projectile.height = 30;
+            Projectile.penetrate = 7;
+            Projectile.DamageType = DamageClass.MeleeNoSpeed;
             Projectile.friendly = true;
             Projectile.hostile = false;
-            Projectile.ignoreWater = true;
+            Projectile.ignoreWater = false;
             Projectile.tileCollide = true;
             Projectile.timeLeft = 190;
-            Projectile.extraUpdates = 2;
+
+            Projectile.aiStyle = -1;
 
             Projectile.localNPCHitCooldown = 10;
             Projectile.usesLocalNPCImmunity = true;
+        }
+
+        public override void OnSpawn(IEntitySource source)
+        {
+            rotMult = Math.Sign(Projectile.velocity.X);
+        }
+
+        ref float rotMult => ref Projectile.ai[0];
+        public override void AI()
+        {
+            Projectile.velocity.Y += 0.2f;
+            Projectile.rotation += 0.2f * rotMult;
+        }
+
+        public override bool OnTileCollide(Vector2 oldVelocity)
+        {
+            Projectile.penetrate--;
+            if (Projectile.penetrate <= 0) return true;
+
+            rotMult *= 0.7f;
+
+            if (Projectile.velocity.X != oldVelocity.X)
+            {
+                Projectile.velocity.X = -oldVelocity.X;
+            }
+            if (Projectile.velocity.Y != oldVelocity.Y)
+            {
+                Projectile.velocity.Y = -oldVelocity.Y;
+            }
+            Projectile.velocity *= 0.6f;
+            SoundEngine.PlaySound(SoundID.Item10, Projectile.position);
+            
+            return false;
+        }
+
+        public override void Kill(int timeLeft)
+        {
+            SoundEngine.PlaySound(SoundID.Shatter with { Volume = 0.4f }, Projectile.Center);
+
+            DarknessFallenUtils.NewDustCircular(Projectile.Center, DustID.Glass, 10, speedFromCenter: 6, amount: Main.rand.Next(10, 24)).ForEach(dust => dust.alpha = Main.rand.Next(0, 220));
+        }
+
+        public override bool PreDraw(ref Color lightColor)
+        {
+            Projectile.DrawProjectileInHBCenter(lightColor, centerOrigin: true);
+            return false;
         }
     }
 }
