120acdf9872e133ec5140cd92e909ebd705b3039
naka <joaovtnakamura@gmail.com>
2022-10-07 00:41:45 -0300

    Safe restart

--- DarknessFallenMod.cs
+++ DarknessFallenMod.cs
@@ -1,33 +1,5 @@
 using Terraria.ModLoader;
-using Terraria;
-using Terraria.ID;
-using Microsoft.Xna.Framework;
-using Terraria.ModLoader.Utilities;
-using DarknessFallenMod.Items.Materials;
-using Terraria.GameContent.ItemDropRules;
-using Terraria.GameContent.Bestiary;
-using System;
-using System.Collections.Generic;
-using Microsoft.Xna.Framework.Graphics;
-using Terraria.GameContent.UI;
-using DarknessFallenMod.Core;
 
-namespace DarknessFallenMod
-{
-	public class DarknessFallenMod : Mod
-	{
-		public static int GelCurrency;
-		public static DarknessFallenMod Instance { get; private set; }
+namespace DarknessFallenMod;
 
-		public override void Load()
-		{
-			Instance = this;
-			// Registers a new custom currency
-			GelCurrency = CustomCurrencyManager.RegisterCurrency(new Systems.Currencies.GelCurrency(ItemID.Gel, 999L, "Gel"));
-		}
-
-		public void LoadLoaders()
-        {
-        }
-	}
-}
+public sealed class DarknessFallenMod : Mod { }
