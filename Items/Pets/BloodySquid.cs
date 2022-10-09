f8de87352f76eec43d1566cef3f7ca8c8fd6277a
TheEternalAce <63331137+TheEternalAce@users.noreply.github.com>
2022-10-08 22:47:57 -0400

    Fixed inability to equip Bloody Tentacle in pet slot

---
+++ Items/Pets/BloodySquid.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace DarknessFallenMod.Items.Pets
+{
+    public class BloodySquid : ModProjectile
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Bloody Squid");
+
+            Main.projFrames[Projectile.type] = 4;
+            Main.projPet[Projectile.type] = true;
+        }
+
+        public override void SetDefaults()
+        {
+            Projectile.width = 30;
+            Projectile.height = 30;
+            Projectile.penetrate = -1;
+            Projectile.netImportant = true;
+            Projectile.friendly = true;
+            Projectile.ignoreWater = true;
+            Projectile.tileCollide = false;
+        }
+
+        public override void AI()
+        {
+            Player player = Main.player[Projectile.owner];
+
+            if (!player.active)
+            {
+                Projectile.active = false;
+                return;
+            }
+
+            if (!player.dead && player.HasBuff(ModContent.BuffType<LichlingBuff>()))
+            {
+                Projectile.timeLeft = 2;
+            }
+
+            Vector2 flyToPos = player.Center + new Vector2(player.direction, 1) * -40;
+            Projectile.Center = Vector2.Lerp(Projectile.Center, flyToPos, 0.2f);
+
+            float rotTo = 0;
+            if (Projectile.DistanceSQ(flyToPos) > 10)
+            {
+                rotTo = Projectile.DirectionTo(flyToPos).X * MathHelper.PiOver2 * 0.8f;
+            }
+            Projectile.rotation = MathHelper.Lerp(Projectile.rotation, rotTo, 0.2f);
+
+            Projectile.spriteDirection = player.direction;
+
+            Projectile.BasicAnimation(5);
+        }
+    }
+}
---
+++ Items/Pets/BloodySquidBuff.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace DarknessFallenMod.Items.Pets
+{
+    public class BloodySquidBuff : ModBuff
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Bloody Squid");
+            Description.SetDefault("squid");
+
+            Main.buffNoTimeDisplay[Type] = true;
+            Main.vanityPet[Type] = true;
+        }
+
+        public override void Update(Player player, ref int buffIndex)
+        {
+            player.buffTime[buffIndex] = 18000;
+
+            int projType = ModContent.ProjectileType<BloodySquid>();
+
+            // If the player is local, and there hasn't been a pet projectile spawned yet - spawn it.
+            if (player.whoAmI == Main.myPlayer && player.ownedProjectileCounts[projType] <= 0)
+            {
+                var entitySource = player.GetSource_Buff(buffIndex);
+
+                Projectile.NewProjectile(entitySource, player.Center, Vector2.Zero, projType, 0, 0f, player.whoAmI);
+            }
+        }
+    }
+}
